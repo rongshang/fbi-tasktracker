657ad54791a243e8bed87fe366ce4b6eaa353c92
hanjianlong <hanjianlong@rongshangsoft.com>
2015-02-13 10:54:53 +0800

    Rename MyWorkOrderInfoMapper.java to MyWorkorderInfoMapper.java

--- src/main/java/task/repository/dao/not_mybatis/MyWorkorderInfoMapper.java
+++ src/main/java/task/repository/dao/not_mybatis/MyWorkorderInfoMapper.java
@@ -11,7 +11,7 @@ import java.util.List;
  * Created by IntelliJ IDEA.
  * User: hanjianlong
  * Date: 13-2-13
- * Time: ÏÂÎç8:10
+ * Time: ÃÃ‚ÃÃ§8:10
  * To change this template use File | Settings | File Templates.
  */
 @Component
